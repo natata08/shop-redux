9b26630a133c944c5bfb9dd6747475dc9098192a
Natalia Lapina <lapinann08@gmail.com>
2025-07-29 19:06:09 +0200

    feat: add cart quantity display with TypeScript-typed Redux hooks

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,12 @@
-import { useDispatch } from 'react-redux'
-import { type AppDispatch } from './store.ts'
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from 'react-redux'
+import type { AppDispatch, RootState } from './store.ts'
 
 type DispatchFunction = () => AppDispatch
 
 export const useCartDispatch: DispatchFunction = useDispatch
+
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector
